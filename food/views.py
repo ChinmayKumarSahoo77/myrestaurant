feec4fb3e2e65f72d106603fa4576f7f6f5f7ad0
Chinmaya Kumar Sahoo <ChSahoo@ESTUATE.COM>
2023-12-06 23:03:30 +0530

    first commit

---
+++ food/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+# function based view
+def first_view(request):
+    return HttpResponse("Hii all, successfully I created my first djnago function based view\
+                        without taking the help of any tutorial.\
+                        A big Thanks to google teacher and the official docs of django")
