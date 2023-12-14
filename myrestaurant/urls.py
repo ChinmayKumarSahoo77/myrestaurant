e48304ceabbc7f28cde49a82834adcce283ba21b
chinmaykumarsahoo77 <chinmayasahoo9777@gmail.com>
2023-12-14 19:52:26 +0530

    Created new application with name 'user' and cretaed the models

--- myrestaurant/urls.py
+++ myrestaurant/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
+    path('user/', include('user.urls')),
 ]
---
+++ user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    location = models.CharField(max_length=1000)
+    image = models.ImageField(default='noimge.jpg', upload_to='pictures')
